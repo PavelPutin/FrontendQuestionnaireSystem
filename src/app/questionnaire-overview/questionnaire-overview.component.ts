695303607e72be1c84cf10a61bf4a8d945726f66
Putin Pavel Alexandrovich <pavelputin2003@yandex.ru>
2023-12-15 12:51:46 +0300

    feat: made base for pagination

--- src/app/questionnaire-overview/questionnaire-overview.component.ts
+++ src/app/questionnaire-overview/questionnaire-overview.component.ts
@@ -17,18 +17,38 @@ import {PaginatedQuestionnaires} from "../paginated-questionnaires";
 })
 export class QuestionnaireOverviewComponent {
   questionnaires: QuestionnaireBrief[] = [];
+  paginationLabels: string[] = [];
   questionnaireService: QuestionnaireService = inject(QuestionnaireService);
 
   constructor() {
   }
 
   ngOnInit() {
+    this.getQuestionnaires()
+  }
+
+  getQuestionnaires(): void {
     this.questionnaireService.getQuestionnaires().then((questionnaires: PaginatedQuestionnaires) => {
       this.questionnaires = questionnaires.briefDTOList;
+      this.paginationLabels = this.updatePaginationLabels(questionnaires);
     })
   }
 
   acceptFilter(questionnaireName: string, authorName: string) {
 
+  }
+
+  private updatePaginationLabels(questionnaires: PaginatedQuestionnaires): string[] {
+    let paginationLabels: string[] = [];
+    if (questionnaires.number < 5) {
+      for (let i = 1; i <= 5 && i <= questionnaires.totalPages; i++) {
+        if (i == 5 && questionnaires.totalPages > 5) {
+          paginationLabels[i - 1] = "...";
+        } else {
+          paginationLabels[i - 1] = i + '';
+        }
+      }
+    }
+    return paginationLabels;
   }
 }
