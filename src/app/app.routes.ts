8fbc726bdd8e07d24c934ce794844ec3305e3bd8
Putin Pavel Alexandrovich <pavelputin2003@yandex.ru>
2023-12-19 13:57:58 +0300

    feat: add routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,34 @@
 import { Routes } from '@angular/router';
 import {QuestionnaireOverviewComponent} from "./questionnaire-overview/questionnaire-overview.component";
+import {LoginComponent} from "./login/login.component";
+import {QuestionnaireDetailComponent} from "./questionnaire-detail/questionnaire-detail.component";
+import {RegistrationComponent} from "./registration/registration.component";
+import {ProfileComponent} from "./profile/profile.component";
 
 export const routes: Routes = [
   {
     path: '',
     component: QuestionnaireOverviewComponent,
     title: "Обзор анкет"
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+    title: "Вход"
+  },
+  {
+    path: 'details/:id',
+    component: QuestionnaireDetailComponent,
+    title: "Анкета"
+  },
+  {
+    path: 'registration',
+    component: RegistrationComponent,
+    title: "Регистрация"
+  },
+  {
+    path: 'profile/:id',
+    component: ProfileComponent,
+    title: "Профиль"
   }
 ];
---
+++ src/app/questionnaire-detail/questionnaire-detail.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-questionnaire-detail',
+  standalone: true,
+  imports: [],
+  templateUrl: './questionnaire-detail.component.html',
+  styleUrl: './questionnaire-detail.component.css'
+})
+export class QuestionnaireDetailComponent {
+
+}
