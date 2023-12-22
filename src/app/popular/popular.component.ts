a35fe01f39fd22e1ae3c44f3efcf189cd37fb811
Putin Pavel Alexandrovich <pavelputin2003@yandex.ru>
2023-12-22 07:55:27 +0300

    fix: add router link to details page in popular component

--- src/app/popular/popular.component.ts
+++ src/app/popular/popular.component.ts
@@ -2,12 +2,14 @@ import {Component, inject} from '@angular/core';
 import {QuestionnaireService} from "../questionnaire.service";
 import {QuestionnaireBrief} from "../questionnaire-brief";
 import {NgForOf} from "@angular/common";
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-popular',
   standalone: true,
   imports: [
-    NgForOf
+    NgForOf,
+    RouterLink
   ],
   templateUrl: './popular.component.html',
   styleUrl: './popular.component.css'
