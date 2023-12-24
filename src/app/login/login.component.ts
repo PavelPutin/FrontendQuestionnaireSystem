767f893888868c669d0f4d90ab66bf48a6467716
Putin Pavel Alexandrovich <pavelputin2003@yandex.ru>
2023-12-24 13:24:33 +0300

    feat: made login component

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
+import {FormControl, FormGroup} from "@angular/forms";
+import {AuthenticationService} from "../authentication.service";
+import {HttpClient} from "@angular/common/http";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-login',
@@ -8,5 +12,22 @@ import { Component } from '@angular/core';
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  loginForm: FormGroup = new FormGroup({
+    username: new FormControl(),
+    password: new FormControl()
+  });
 
+  authenticationService: AuthenticationService = inject(AuthenticationService);
+  http: HttpClient = inject(HttpClient);
+  router: Router = inject(Router);
+
+  login() {
+    console.log(this.loginForm.value.username);
+    console.log(this.loginForm.value.password);
+    this.authenticationService.authenticate({
+      username: this.loginForm.value.username ?? '', password: this.loginForm.value.password ?? ''}, () => {
+      this.router.navigateByUrl("/").then(v => {console.log(v)});
+    });
+    return false;
+  }
 }
