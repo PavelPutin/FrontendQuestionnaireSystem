83665b6d4a631beae80384f98e6b5958403e5bc7
Putin Pavel Alexandrovich <pavelputin2003@yandex.ru>
2023-12-25 13:37:31 +0300

    feat: made registration

---
+++ src/app/registration/registration-data.ts
@@ -0,0 +1,10 @@
+import {FormControl} from "@angular/forms";
+
+export interface RegistrationData {
+  username: string,
+  password: string,
+  age: number
+  gender: string,
+  maritalStatus: string,
+  country: string
+}
--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -3,6 +3,8 @@ import {FormControl, FormGroup, ReactiveFormsModule} from "@angular/forms";
 import {NgForOf} from "@angular/common";
 import {CountryService} from "../country.service";
 import {Country} from "../country";
+import {RegistrationService} from "../registration.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-registration',
@@ -27,11 +29,27 @@ export class RegistrationComponent {
   countryService: CountryService = inject(CountryService);
   countries: Country[] = [];
 
-  constructor() {}
+  constructor(
+    private registrationService: RegistrationService,
+    private router: Router) {
+  }
 
   ngOnInit() {
     this.countryService.getCountries().subscribe(countries => {
       this.countries = countries;
     })
+  }
+
+  submitRegistration() {
+    const registrationData = {
+      username: this.registrationForm.value.username,
+      password: this.registrationForm.value.password,
+      age: this.registrationForm.value.age,
+      gender: this.registrationForm.value.gender,
+      maritalStatus: this.registrationForm.value.maritalStatus,
+      country: this.registrationForm.value.country
+    }
+    this.registrationService.registerUser(registrationData);
+    this.router.navigateByUrl("/").then();
   }
 }
