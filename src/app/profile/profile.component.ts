ffc57d1c53c20a5c8800e262cbb2cdc69de8e06d
Putin Pavel Alexandrovich <pavelputin2003@yandex.ru>
2023-12-25 16:35:38 +0300

    feat: add information about user

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
+import {AuthenticationService} from "../authentication.service";
+import {User} from "../user";
+import {ActivatedRoute, Router} from "@angular/router";
+import {UserService} from "../user.service";
 
 @Component({
   selector: 'app-profile',
@@ -8,5 +12,35 @@ import { Component } from '@angular/core';
   styleUrl: './profile.component.css'
 })
 export class ProfileComponent {
+  route: ActivatedRoute = inject(ActivatedRoute);
+  router: Router = inject(Router);
+  user: User | undefined;
+  maritalStatus: string = "";
+  internalizationMaritalStatus = {
+    "male": {
+      married: 'женат',
+      divorced: 'разведён',
+      was_not_married: 'не был женат'
+    },
+    "female": {
+      married: 'замужем',
+      divorced: 'разведёна',
+      was_not_married: 'не был замужем'
+    }
+  }
 
+  constructor(private userService: UserService) {
+    let userId = this.route.snapshot.params['id'];
+    this.userService.getById(userId).subscribe(user => {
+      this.user = user;
+      if (typeof user !== "undefined") {
+        // @ts-ignore
+        this.maritalStatus = this.internalizationMaritalStatus[user.gender][user.maritalStatus];
+      }
+    });
+  }
+
+  ngOnInit() {
+
+  }
 }
